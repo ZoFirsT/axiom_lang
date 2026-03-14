47152065a78eb5f724072c66421a92edbfe0cb4e
Bangkok University <bangkokuniversity@Thanatchas-MacBook-Pro.local>
2026-03-14 13:36:53 +0700

    fix bug edition

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Token {
+    Let,                 
+    Identifier(String),  
+    Assign,              
+    Number(i64),         
+    Semicolon,           
+    Illegal,             
+    EOF,                 
+}
+
+pub struct Lexer {
+    input: String,       
+    position: usize,     
+    read_position: usize,
+    ch: char,            
+}
+
+impl Lexer {
+
+    pub fn new(input: String) -> Self {
+        let mut lexer = Lexer {
+            input,
+            position: 0,
+            read_position: 0,
+            ch: '\0',
+        };
+        lexer.read_char();
+        lexer
+    }
+
+
+    fn read_char(&mut self) {
+        if self.read_position >= self.input.len() {
+            self.ch = '\0';
+        } else {
+            self.ch = self.input.chars().nth(self.read_position).unwrap();
+        }
+        self.position = self.read_position;
+        self.read_position += 1;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the Axiom Compiler!");
+
 }
